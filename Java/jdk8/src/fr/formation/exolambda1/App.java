938961d5a58ecefdb7208d5dc533cda3f94ddad9
Jeremy <jeremy.perrouault@ascadis.fr>
2022-05-17 10:43:06 +0200

    JOUR 1 - EXO LAMBDA 1 - USE CASE

--- Java/jdk8/src/fr/formation/exolambda1/App.java
+++ Java/jdk8/src/fr/formation/exolambda1/App.java
@@ -3,8 +3,19 @@ package fr.formation.exolambda1;
 public class App {
     public static void main(String[] args) {
         // Math math = (int a, int b) -> a + b; // Le type est possible
-        Math math = (a, b) -> a + b;
+        // Math math = (a, b) -> a + b;
 
-        System.out.println(math.operation(5, 10));
+        // System.out.println(math.operation(5, 10));
+
+        useLambda(
+            (a, b) -> a * b,
+            15,
+            30
+        );
+    }
+
+
+    public static void useLambda(Math m, int a, int b) {
+        System.out.println(m.operation(a, b));
     }
 }
